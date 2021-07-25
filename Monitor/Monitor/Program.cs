8791a3047b4f15e261f252874d9ef7de53fdce10
Alex Valchuk <alex.valchuk@deltatre.it>
2021-07-25 17:53:17 +0300

    Monitor sample

---
+++ Monitor/Monitor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitorExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task.Run(() =>
+            {
+                var singleton = Singleton.GetSingletonUsingMonitorExplicitly();
+                singleton.ShowId("Thread1");
+            });
+
+            Task.Run(() =>
+            {
+                var singleton = Singleton.GetSingletonUsingMonitorImplicitly();
+                singleton.ShowId("Thread2");
+            });
+            
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ Monitor/Monitor/Singleton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace MonitorExample
+{
+    public class Singleton
+    {
+        private static readonly Object s_lock = new object();
+
+        private static Singleton singleton;
+        private readonly int _id;
+
+        private Singleton()
+        {
+            _id = new Random().Next(0, 100);
+        }
+
+        public static Singleton GetSingletonUsingMonitorExplicitly()
+        {
+            if (singleton != null)
+            {
+                return singleton;
+            }
+
+            Monitor.Enter(s_lock);
+
+            try
+            {
+                singleton ??= new Singleton();
+            }
+            finally
+            {
+                Monitor.Exit(s_lock);
+            }
+            
+            return singleton;
+        }
+
+        public static Singleton GetSingletonUsingMonitorImplicitly()
+        {
+            if (singleton != null)
+            {
+                return singleton;
+            }
+
+            lock (s_lock)
+            {
+                singleton ??= new Singleton();
+            }
+            
+            return singleton;
+        }
+
+        public void ShowId(string threadName)
+        {
+            Console.WriteLine($"The Id of a singleton for thread {threadName} is {_id}.");
+        }
+    }
+}
