665261cc807c6bdd9a18eb3919e6a84f11c92bc9
Alex Valchuk <alex.valchuk@deltatre.it>
2021-07-27 19:14:07 +0300

    BroadcastBlock sample

---
+++ BroadcastBlock/BroadcastBlock/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace BroadcastBlockExample
+{
+    static class Program
+    {
+        static int Delay = 500;
+        static async Task Main()
+        {
+            var broadcastBlock = new BroadcastBlock<string>(null);
+
+            var sendTask = Task.Run(() =>
+            {
+                broadcastBlock.Post("Enemy spotted!");
+                Thread.Sleep(Delay);
+                broadcastBlock.Post("Fire in the hole!");
+                Thread.Sleep(Delay);
+                broadcastBlock.Post("Affirmative!");
+                Thread.Sleep(Delay);
+            
+                broadcastBlock.Complete();
+            });
+
+            var receiveTask = Task.Run(() =>
+            {
+                while (!broadcastBlock.Completion.IsCompleted)
+                {
+                    Console.WriteLine("Received:" + broadcastBlock.Receive());
+                    Thread.Sleep(Delay);
+                }
+            });
+
+            await Task.WhenAll(sendTask, receiveTask);
+        }
+    }
+}
