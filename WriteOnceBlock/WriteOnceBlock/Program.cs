61fd26c1ab34a326c10fb43c32fe7b7e4ae94f4e
Alex Valchuk <alex.valchuk@deltatre.it>
2021-07-28 09:47:06 +0300

    WriteOnceBlock sample

---
+++ WriteOnceBlock/WriteOnceBlock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace WriteOnceBlockExample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var writeOnceBlock = new WriteOnceBlock<string>(null);
+            
+            Parallel.Invoke(
+                () => writeOnceBlock.Post("Fire in the hole!"),
+                () => writeOnceBlock.Post("Enemy spotted!"),
+                () => writeOnceBlock.Post("Affirmative!"));
+            
+            Console.WriteLine(writeOnceBlock.Receive());
+        }
+    }
+}
