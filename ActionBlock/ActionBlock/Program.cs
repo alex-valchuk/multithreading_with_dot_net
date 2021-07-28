15ee364854fe398072393031ed55701d985146bd
Alex Valchuk <alex.valchuk@deltatre.it>
2021-07-28 09:51:32 +0300

    ActionBlock sample

---
+++ ActionBlock/ActionBlock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace ActionBlockExample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var actionBlock = new ActionBlock<int>(Console.WriteLine);
+
+            for (int i = 0; i < 3; i++)
+            {
+                actionBlock.Post(i);
+            }
+
+            actionBlock.Complete();
+            actionBlock.Completion.Wait();
+        }
+    }
+}
